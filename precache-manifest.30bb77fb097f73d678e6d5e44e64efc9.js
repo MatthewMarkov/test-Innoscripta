8f0d450315b6baeab05a8b5e9613b63088ab65c1
MatthewMarkov <mathew.markov@gmail.com>
2020-06-09 13:32:22 +0300

    Updates

---
+++ precache-manifest.30bb77fb097f73d678e6d5e44e64efc9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2bbd810b40410e6cc97036633de46ad",
+    "url": "/test-Innoscripta/index.html"
+  },
+  {
+    "revision": "fcfed47ef51561e66d91",
+    "url": "/test-Innoscripta/static/css/main.0a7aa6ec.chunk.css"
+  },
+  {
+    "revision": "12a7becb31b50636d3f2",
+    "url": "/test-Innoscripta/static/js/2.8764fea3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-Innoscripta/static/js/2.8764fea3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcfed47ef51561e66d91",
+    "url": "/test-Innoscripta/static/js/main.4e7eae3b.chunk.js"
+  },
+  {
+    "revision": "4355be0645576b73ec7a",
+    "url": "/test-Innoscripta/static/js/runtime-main.1c2fb66b.js"
+  },
+  {
+    "revision": "4da63c4d4f7bed7bbfdbd82a631894b3",
+    "url": "/test-Innoscripta/static/media/innoscripta pizza.4da63c4d.png"
+  },
+  {
+    "revision": "054e0dca269e0ce3ad0e3449dc762de0",
+    "url": "/test-Innoscripta/static/media/pepperoni.054e0dca.png"
+  },
+  {
+    "revision": "0e08f63edce3f0e8410c28abca78c1ac",
+    "url": "/test-Innoscripta/static/media/pngwing.com (1).0e08f63e.png"
+  },
+  {
+    "revision": "5d9a1e190fe4b8913b29d8d9d0ae2d98",
+    "url": "/test-Innoscripta/static/media/pngwing.com (2).5d9a1e19.png"
+  },
+  {
+    "revision": "83709c0569b69036b931586ee5e139a1",
+    "url": "/test-Innoscripta/static/media/pngwing.com (3).83709c05.png"
+  },
+  {
+    "revision": "cfad6b25752a218dfb9ef53da4875970",
+    "url": "/test-Innoscripta/static/media/pngwing.com (4).cfad6b25.png"
+  },
+  {
+    "revision": "e9afc071c4bdaf52367c73fbd4f17739",
+    "url": "/test-Innoscripta/static/media/pngwing.com (5).e9afc071.png"
+  },
+  {
+    "revision": "f499b4aa3e04c74d7bea2409529e10d0",
+    "url": "/test-Innoscripta/static/media/pngwing.com (6).f499b4aa.png"
+  },
+  {
+    "revision": "c67e3c8caf6721ac90220ad6d7969602",
+    "url": "/test-Innoscripta/static/media/pngwing.com (7).c67e3c8c.png"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/test-Innoscripta/static/media/shopping-cart-solid.8bf1af37.svg"
+  }
+]);
